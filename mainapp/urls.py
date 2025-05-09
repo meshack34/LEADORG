a392fe9183efbec8fd7fdacdde00497f948a527d
meshack34 <meshackkimutai34.com>
2025-05-09 10:07:53 +0300

    update

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -14,7 +14,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.payment_view, name='payment'),
+    path('payment', views.payment_view, name='payment'),
     path('callback/', views.payment_callback, name='payment_callback'),
     path('stk-status/', views.stk_status_view, name='stk_status'),
     
